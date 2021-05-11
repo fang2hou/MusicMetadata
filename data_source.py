4745157f93e04b8fbb187c31093d6d30af4067df
Zhou Fang <fang2hou@outlook.com>
2021-05-12 03:20:15 +0900

    add support of Apple Music.

--- data_source.py
+++ data_source.py
@@ -2,6 +2,7 @@ from abc import ABCMeta, abstractmethod, ABC
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup
+from datetime import datetime
 import chromedriver_binary
 
 
@@ -42,6 +43,49 @@ class Mora(DataSource):
                     song['artist'] = cell.text
                 if "<!--楽曲名-->" in str(cell):
                     song['name'] = cell.text
+            data['songs'][song['id']] = song
+
+        return data
+
+
+class AppleMusic(DataSource):
+    def __init__(self, chromedriver_path: str = None):
+        super(AppleMusic, self).__init__()
+        options = webdriver.ChromeOptions()
+        options.headless = True
+        if chromedriver_path:
+            options.binary_location = chromedriver_path
+        self.driver = webdriver.Chrome(chrome_options=options)
+
+    def get_artists_string(self, line):
+        artists = []
+        for artist_link in line.select('a'):
+            artists.append(artist_link.text)
+        return "; ".join(artists)
+
+    def get_data_by_url(self, data_url: str) -> list:
+        self.driver.get(url=data_url)
+        WebDriverWait(self.driver, 10)
+        soup = BeautifulSoup(self.driver.page_source, "html.parser")
+
+        data = dict()
+
+        data['album'] = soup.select("h1.product-name")[0].text.strip()
+        artist_div = soup.select("div.product-creator")[0]
+        data['album_artist'] = self.get_artists_string(artist_div)
+        release_date = datetime.strptime(soup.select("p.song-released-container")[0].text.strip(), "%B %d, %Y")
+        data['release_date'] = release_date.strftime("%Y.%m.%d")
+
+        data['songs'] = dict()
+        table_body = soup.select("div.songs-list")[0]
+        for line in table_body.select("div.songs-list-row"):
+            song = dict()
+            song['id'] = line.select('span.songs-list-row__column-data')[0].text.strip()
+            if len(line.select('div.songs-list-row__by-line')) > 0:
+                song['artist'] = self.get_artists_string(line.select('div.songs-list-row__by-line')[0])
+            else:
+                song['artist'] = data['album_artist']
+            song['name'] = line.select('div.songs-list-row__song-name')[0].text.strip()
             data['songs'][song['id']] = song
 
         return data
